abc47bfa416df0f5b252f263af51757be6bf59f0
cgm <cgm@example.com>
2019-04-22 11:15:33 +0800

    测试接口

---
+++ src/main/java/com/cgm/controller/Test1.java
@@ -0,0 +1,11 @@
+package com.cgm.controller;
+
+/**
+ * @author: CGM
+ * @date: 2019/4/22 11:12
+ */
+public interface Test1 {
+    default  void youqu(){
+        System.out.println("你是猪吗！");
+    }
+}
